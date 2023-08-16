7d3ad02c1a456c23c73087758fc8f4f66f559ed5
Gonzalo Lascar <gonzalolascar@gmail.com>
2023-08-16 00:13:24 -0300

    test: add unit tests to users controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,6 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { usersProviders } from './entity/users.providers';
+import { getModelToken } from '@nestjs/sequelize';
+import { User } from './entity/user.entity';
+
+const newUserMock = {
+  name: 'Joe Doe',
+  email: 'joe.doe@sample.com',
+  username: 'Joe569',
+  password: 'my-pass-456',
+};
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -17,5 +26,28 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('Create user', () => {
+    it('Should return created true response for a valid user', async () => {
+      User.findOne = jest.fn().mockResolvedValue(null);
+      User.create = jest.fn().mockResolvedValue(true);
+      const userCreatedResponse = await controller.registerUser(newUserMock);
+      const expectedResponse = {
+        created: true,
+      };
+      expect(userCreatedResponse).toEqual(expectedResponse);
+    });
+
+    it('Should return exception if an user already exists', async () => {
+      User.findOne = jest.fn().mockResolvedValue(newUserMock);
+      try {
+        await controller.registerUser(newUserMock);
+      } catch (exception) {
+        const expectedErrorMessage = `Cannot register this user. Email ${newUserMock.email} already registered`;
+        expect(exception.response.error).toEqual(expectedErrorMessage);
+        expect(exception.response.status).toEqual(400);
+      }
+    });
   });
 });
