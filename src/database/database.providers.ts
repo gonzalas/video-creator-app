d1f995d75043823346a6cf56140761b842c5d14b
Gonzalo Lascar <gonzalolascar@gmail.com>
2023-08-15 23:01:31 -0300

    fix: change sequelize port to 5432

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -12,7 +12,7 @@ export const databaseProviders = [
       const sequelize = new Sequelize({
         dialect: 'postgres',
         host: 'localhost',
-        port: 5433,
+        port: 5432,
         username: 'postgres',
         password: 'postgres',
         database: 'postgres',
